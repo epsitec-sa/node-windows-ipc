097af9a7d3324be593a11b8d2c90c1edba9aad2f
Jonny Quarta <quarta@epsitec.ch>
2021-04-19 13:29:18 +0200

    Write simple test for openSharedMemory function

--- test/index.js
+++ test/index.js
@@ -3,13 +3,14 @@
 var assert = require("assert");
 var lib = require("../");
 
-describe("ExecuteCommand", function () {
-  it("should receive command", function () {
-    // TODO: start udater service
-    lib.executeCommand("CresusUpdaterService", 200); // CheckAvailable command
-  });
+describe("OpenSharedMemory", function () {
+  it("should open shared memory", function () {
+    const handle = lib.openSharedMemory(
+      "Global\\CresusUpdaterServiceStateSharedMemory",
+      lib.sharedMemoryAccess.Read,
+      4096
+    );
 
-  it("should not connect to service", function () {
-    assert.throws(() => lib.executeCommand("DummyService", 200));
+    assert.ok(handle);
   });
 });
