f59172b86555be9f334a98def19813c2912f96c9
Jonny Quarta <quarta@epsitec.ch>
2021-04-19 14:06:33 +0200

    Add createSharedMemory and fix openSharedMemory

--- module.js
+++ module.js
@@ -1,11 +1,31 @@
 const sharedMemoryAddon = require("./build/Release/sharedMemory");
 
-function openSharedMemory(name, sharedMemoryAccess, memorySize) {
+function createSharedMemory(name, pageAccess, fileMapAccess, memorySize) {
+  const handle = Buffer.alloc(sharedMemoryAddon.sizeof_SharedMemoryHandle);
+
+  const res = sharedMemoryAddon.CreateSharedMemory(
+    name,
+    pageAccess,
+    fileMapAccess,
+    memorySize,
+    handle
+  );
+
+  if (res > 0) {
+    throw `could not create file mapping for object ${name}: ${res}`;
+  } else if (res < 0) {
+    throw `could not map view of file ${name}: ${0 - res}`;
+  }
+
+  return handle;
+}
+
+function openSharedMemory(name, fileMapAccess, memorySize) {
   const handle = Buffer.alloc(sharedMemoryAddon.sizeof_SharedMemoryHandle);
 
   const res = sharedMemoryAddon.OpenSharedMemory(
     name,
-    sharedMemoryAccess,
+    fileMapAccess,
     memorySize,
     handle
   );
@@ -13,17 +33,24 @@ function openSharedMemory(name, sharedMemoryAccess, memorySize) {
   if (res > 0) {
     throw `could not open file mapping for object ${name}: ${res}`;
   } else if (res < 0) {
-    throw `could not map view of file ${name}: ${res}`;
+    throw `could not map view of file ${name}: ${0 - res}`;
   }
 
   return handle;
 }
 
 module.exports = {
+  createSharedMemory,
   openSharedMemory,
-  sharedMemoryAccess: {
+
+  sharedMemoryPageAccess: {
+    ReadOnly: 0x02,
+    WriteCopy: 0x08,
+    ReadWrite: 0x04,
+  },
+  sharedMemoryFileMapAccess: {
     Read: 0x0004,
     Write: 0x0002,
-    ReadWrite: 0x0004 | 0x0002,
+    AllAccess: 0xf001f,
   },
 };
--- src/sharedMemory.cpp
+++ src/sharedMemory.cpp
@@ -10,33 +10,63 @@ struct SharedMemoryHandle
   LPCTSTR pBuf;
 };
 
-// string name, int access, int memSize, SharedMemory* memoryHandle -> int
-NAPI_METHOD(OpenSharedMemory)
+// string name, int pageAccess, int fileMapAccess, int memSize, SharedMemory* memoryHandle -> int
+NAPI_METHOD(CreateSharedMemory)
 {
-  HANDLE hMapFile;
-  LPCTSTR pBuf;
   int result = 0;
 
-  NAPI_ARGV(4)
+  NAPI_ARGV(5)
 
   NAPI_ARGV_UTF8(objectNameA, 1000, 0)
-  NAPI_ARGV_INT32(sharedMemoryAccess, 1)
-  NAPI_ARGV_INT32(memorySize, 2)
-  NAPI_ARGV_BUFFER_CAST(struct SharedMemoryHandle *, memoryHandle, 3)
+  NAPI_ARGV_INT32(sharedMemoryPageAccess, 1)
+  NAPI_ARGV_INT32(sharedMemoryFileMapAccess, 2)
+  NAPI_ARGV_INT32(memorySize, 3)
+  NAPI_ARGV_BUFFER_CAST(struct SharedMemoryHandle *, memoryHandle, 4)
 
-  memoryHandle->hMapFile = OpenFileMapping(sharedMemoryAccess, FALSE, objectNameA);
+  memoryHandle->hMapFile = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, sharedMemoryPageAccess, 0, memorySize, objectNameA);
   if (memoryHandle->hMapFile == NULL)
   {
     result = GetLastError();
     NAPI_RETURN_INT32(result)
   }
 
-  memoryHandle->pBuf = (LPTSTR)MapViewOfFile(hMapFile, sharedMemoryAccess, 0, 0, memorySize);
+  memoryHandle->pBuf = (LPTSTR)MapViewOfFile(memoryHandle->hMapFile, sharedMemoryFileMapAccess, 0, 0, memorySize);
   if (memoryHandle->pBuf == NULL)
   {
     CloseHandle(memoryHandle->hMapFile);
 
-    result = -1 * GetLastError();
+    result = 0 - GetLastError();
+    NAPI_RETURN_INT32(result);
+  }
+
+  NAPI_RETURN_INT32(result)
+}
+
+// string name, int fileMapAccess, int memSize, SharedMemory* memoryHandle -> int
+NAPI_METHOD(OpenSharedMemory)
+{
+  int result = 0;
+
+  NAPI_ARGV(4)
+
+  NAPI_ARGV_UTF8(objectNameA, 1000, 0)
+  NAPI_ARGV_INT32(sharedMemoryPageAccess, 1)
+  NAPI_ARGV_INT32(memorySize, 2)
+  NAPI_ARGV_BUFFER_CAST(struct SharedMemoryHandle *, memoryHandle, 3)
+
+  memoryHandle->hMapFile = OpenFileMapping(sharedMemoryPageAccess, FALSE, objectNameA);
+  if (memoryHandle->hMapFile == NULL)
+  {
+    result = GetLastError();
+    NAPI_RETURN_INT32(result)
+  }
+
+  memoryHandle->pBuf = (LPTSTR)MapViewOfFile(memoryHandle->hMapFile, sharedMemoryPageAccess, 0, 0, memorySize);
+  if (memoryHandle->pBuf == NULL)
+  {
+    CloseHandle(memoryHandle->hMapFile);
+
+    result = 0 - GetLastError();
     NAPI_RETURN_INT32(result);
   }
 
@@ -45,7 +75,9 @@ NAPI_METHOD(OpenSharedMemory)
 
 NAPI_INIT()
 {
+  NAPI_EXPORT_FUNCTION(CreateSharedMemory)
   NAPI_EXPORT_FUNCTION(OpenSharedMemory)
+
   NAPI_EXPORT_SIZEOF_STRUCT(SharedMemoryHandle)
   NAPI_EXPORT_ALIGNMENTOF(SharedMemoryHandle)
 }
